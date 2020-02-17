249ba540657ab255966fde858afc1a6c2a4ab59c
Mario Mendes <mariuu.34@gmail.com>
2020-02-17 20:54:36 -0300

    Criacao EventClick

---
+++ src/gui/ViewController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+	
+	@FXML
+	private Button btTest;
+	
+	public void onBtTestAction() {
+		
+		System.out.println("Click");
+		
+	}
+
+}
